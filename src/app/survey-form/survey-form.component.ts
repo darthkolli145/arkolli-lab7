ca9c3765800f170bf8c2ba0cc2c3b3b181ecea98
darthkolli145 <77557182+darthkolli145@users.noreply.github.com>
2024-11-10 22:44:32 -0800

    typescript

--- src/app/survey-form/survey-form.component.ts
+++ src/app/survey-form/survey-form.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-survey-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './survey-form.component.html',
-  styleUrl: './survey-form.component.scss'
+  styleUrls: ['./survey-form.component.scss']
 })
 export class SurveyFormComponent {
+  surveyForm: FormGroup;
+  @Output() surveySubmitted = new EventEmitter<any>();
 
+  constructor(private fb: FormBuilder) {
+    this.surveyForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      age: ['', [Validators.required, Validators.min(1)]],
+      feedback: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.surveyForm.valid) {
+      this.surveySubmitted.emit(this.surveyForm.value);
+      this.surveyForm.reset();
+    }
+  }
+
+  get name() { return this.surveyForm.get('name'); }
+  get email() { return this.surveyForm.get('email'); }
+  get age() { return this.surveyForm.get('age'); }
+  get feedback() { return this.surveyForm.get('feedback'); }
 }
+
