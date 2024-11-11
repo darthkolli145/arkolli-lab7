c509dd2ba522b40d1924af126b5e8302db08b1c8
darthkolli145 <77557182+darthkolli145@users.noreply.github.com>
2024-11-10 22:45:34 -0800

    type script

--- src/app/survey-table/survey-table.component.ts
+++ src/app/survey-table/survey-table.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-survey-table',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './survey-table.component.html',
-  styleUrl: './survey-table.component.scss'
+  styleUrls: ['./survey-table.component.scss']
 })
 export class SurveyTableComponent {
-
+  @Input() surveyResponses: any[] = [];
 }
